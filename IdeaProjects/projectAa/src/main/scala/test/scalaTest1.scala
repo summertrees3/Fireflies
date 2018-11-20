4f3f3a7525b5bd217e489880bc1d8f504fe0b209
hoperun_000008714 <ding_weiguo@hoperun.com>
2018-11-20 18:16:11 +0800

    M

--- IdeaProjects/projectAa/src/main/scala/test/scalaTest1.scala
+++ IdeaProjects/projectAa/src/main/scala/test/scalaTest1.scala
@@ -1,21 +1,37 @@
 package test
 
+import java.text.SimpleDateFormat
+import java.util.{Calendar, Date}
+
 object scalaTest1 {
 
   def main(args: Array[String]): Unit = {
 
     //1
-//    def formatArgs(args: Array[String]) = args.mkString("\n")
-//    println(formatArgs(Array("zero","one","two")))
+    //    def formatArgs(args: Array[String]) = args.mkString("\n")
+    //    println(formatArgs(Array("zero","one","two")))
 
 
     //柯里化
-    def curriedSum(x:Int)(y:Int)=x+y
-    println(curriedSum(1)(2))
+    //    def curriedSum(x:Int)(y:Int)=x+y
+    //    println(curriedSum(1)(2))
 
 
-    //2
+    //2 时间
+//    val now: Date = new Date()
+//    val dateFormat: SimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+//    val date = dateFormat.format(now)
+//    println(date)
+//
+//    val cal: Calendar = Calendar.getInstance()
+//    println(cal.getTime)
+//    cal.add(Calendar.DATE, 99)
+//    val nextdate = dateFormat.format(cal.getTime())
+//    println(nextdate)
 
+
+    //3
+    
 
   }
 }
