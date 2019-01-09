6d9b53af3a1c9d34e9c6d73cf091b4acb1c83a9a
hoperun_000008714 <ding_weiguo@hoperun.com>
2019-01-09 09:10:41 +0800

    m

--- PyProjects/projectAa/helloWorld.py
+++ PyProjects/projectAa/helloWorld.py
@@ -7,14 +7,16 @@
 # print(add(-3, 5, abs))
 
 
-# from functools import reduce
-# # def fn(x,y):
-# #     return x * 10 + y
-# def char2num(s):
-#     digits={'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
-#     return digits[s]
-# print(reduce(lambda x, y: x * 10 + y,map(char2num,'13579')))
-
+# 多行注释
+'''
+from functools import reduce
+# def fn(x,y):
+#     return x * 10 + y
+def char2num(s):
+    digits={'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
+    return digits[s]
+print(reduce(lambda x, y: x * 10 + y,map(char2num,'13579')))
+'''
 
 # def is_odd(n):
 #     return n % 2 == 1
